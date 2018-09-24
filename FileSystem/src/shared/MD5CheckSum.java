b0e7147698ece304bd0ed6646e5f15f0e2d142e9
Vincent <vincent.chasse@polymtl.ca>
2018-09-24 15:36:51 -0400

    Add MD5 checksum generator

---
+++ FileSystem/src/shared/MD5CheckSum.java
@@ -0,0 +1,18 @@
+package shared;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import javax.xml.bind.DatatypeConverter;
+
+public final class MD5CheckSum {
+
+    public static String generateChecksum(String filePath) throws IOException, NoSuchAlgorithmException {
+        byte[] fileContents = Files.readAllBytes(Paths.get(filePath));
+        byte[] md5Hash = MessageDigest.getInstance("MD5").digest(fileContents);
+        return DatatypeConverter.printHexBinary(md5Hash);
+    }
+}
