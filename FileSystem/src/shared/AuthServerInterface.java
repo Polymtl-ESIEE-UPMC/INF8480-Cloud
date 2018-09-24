1fa219118188dc5ea9b9f5651c5c558ccf941b80
Fudgyking <escalado19@hotmail.com>
2018-09-24 00:23:08 -0400

    Use Account in server interfaces

--- FileSystem/src/shared/AuthServerInterface.java
+++ FileSystem/src/shared/AuthServerInterface.java
@@ -5,5 +5,5 @@ import java.rmi.RemoteException;
 
 public interface AuthServerInterface extends Remote {
 	boolean newAccount(String login, String password) throws RemoteException;
-	boolean verifyAccount(String login, String password) throws RemoteException;
+	boolean verifyAccount(Account account) throws RemoteException;
 }
--- FileSystem/src/shared/FileServerInterface.java
+++ FileSystem/src/shared/FileServerInterface.java
@@ -4,11 +4,11 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface FileServerInterface extends Remote {
-	void createFile(String fileName) throws RemoteException;
-	String[] listFiles() throws RemoteException;
+	void createFile(Account account, String fileName) throws RemoteException;
+	String[] listFiles(Account account) throws RemoteException;
 	//https://stackoverflow.com/questions/304268/getting-a-files-md5-checksum-in-java
-	byte[] getFile(String name, String checksum) throws RemoteException;
-	String lockFile(String name, String checksum) throws RemoteException;
-	String pushFile(String name, byte[] fileContent) throws RemoteException;
-	byte[] syncLocalDirectory() throws RemoteException;
+	byte[] getFile(Account account, String name, String checksum) throws RemoteException;
+	String lockFile(Account account, String name, String checksum) throws RemoteException;
+	String pushFile(Account account, String name, byte[] fileContent) throws RemoteException;
+	byte[] syncLocalDirectory(Account account) throws RemoteException;
 }
