2a483080b7dfb48b755c73a3a85c62700bdaf52f
Fudgyking <escalado19@hotmail.com>
2018-09-24 00:18:47 -0400

    Removed useless help message in client.sh

--- FileSystem/client.sh
+++ FileSystem/client.sh
@@ -4,11 +4,4 @@ pushd $(dirname $0) > /dev/null
 basepath=$(pwd)
 popd > /dev/null
 
-cat << EndOfMessage
-HELP: 
-./client.sh ip_address
-	- remote_server_ip: (OPTIONAL) l'addresse ip du serveur distant
-
-EndOfMessage
-
 java -cp "$basepath"/client.jar:"$basepath"/shared.jar -Djava.security.policy="$basepath"/policy client.Client $*
