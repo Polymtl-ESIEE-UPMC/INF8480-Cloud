ce52126d15dec1022c428aca419b8e7d9ff5b8c1
Vincent <vincent.chasse@polymtl.ca>
2018-09-24 16:15:53 -0400

    Improve exception handling in MD5CheckSum

--- FileSystem/src/shared/MD5CheckSum.java
+++ FileSystem/src/shared/MD5CheckSum.java
@@ -10,12 +10,12 @@ import javax.xml.bind.DatatypeConverter;
 
 public final class MD5CheckSum {
 
-    public static String generateChecksum(String filePath) throws NoSuchAlgorithmException {
+    public static String generateChecksum(String filePath) {
         try {
             byte[] fileContents = Files.readAllBytes(Paths.get(filePath));
             byte[] md5Hash = MessageDigest.getInstance("MD5").digest(fileContents);
             return DatatypeConverter.printHexBinary(md5Hash);
-        } catch (IOException e) {
+        } catch (IOException | NoSuchAlgorithmException e) {
             return null;
         }
     }
