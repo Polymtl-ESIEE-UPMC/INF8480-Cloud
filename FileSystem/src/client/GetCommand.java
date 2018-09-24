94d258d8910f78d62ac0c575f8aa53ca6ef6fa1a
Vincent <vincent.chasse@polymtl.ca>
2018-09-24 15:39:55 -0400

    Début d'implémentation de la commande get

--- FileSystem/src/client/GetCommand.java
+++ FileSystem/src/client/GetCommand.java
@@ -1,13 +1,25 @@
 package client;
 
 import shared.Account;
+
+import java.io.IOException;
 import java.rmi.RemoteException;
+import java.security.NoSuchAlgorithmException;
+
 import shared.AuthServerInterface;
 import shared.FileServerInterface;
+import shared.MD5CheckSum;
 
 public class GetCommand extends Command{
 
     public void run(Account account, FileServerInterface fileServer, String fileName) throws RemoteException{
-
+        try{
+            String md5 = MD5CheckSum.generateChecksum(fileName);
+            fileServer.getFile(account, fileName, md5);
+        } catch (IOException e){
+            System.err.println(fileName + " n'existe pas sur votre disque!");
+        } catch (NoSuchAlgorithmException e){
+            System.err.println(e.getMessage());
+        }
     }
 }
