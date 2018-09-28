5a18eb76e3665c25bcb5255f8ba61d53e667be30
Fudgyking <escalado19@hotmail.com>
2018-09-28 17:29:23 -0400

    Formattage et modification des imports dans Fichier.java

--- FileSystem/src/shared/Fichier.java
+++ FileSystem/src/shared/Fichier.java
@@ -1,9 +1,14 @@
 package shared;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.Serializable;
-import java.io.*;
 
-public class Fichier implements Serializable{
+public class Fichier implements Serializable {
     private static final String FILES_DIR_NAME = "files";
 
     public String name;
@@ -11,22 +16,22 @@ public class Fichier implements Serializable{
     public String lock;
     public String lockByUser = "";
 
-    public Fichier(String name, byte[] content){
+    public Fichier(String name, byte[] content) {
         this.name = name;
         this.content = content;
-        this.lock = FILES_DIR_NAME+"/."+name+"_lockstate";
+        this.lock = FILES_DIR_NAME + "/." + name + "_lockstate";
     }
 
-    public Fichier(String name, boolean legacy, String legacy1){
+    public Fichier(String name, boolean legacy, String legacy1) {
         this.name = name;
         this.content = new byte[0];
-        this.lock = FILES_DIR_NAME+"/."+name+"_lockstate";
+        this.lock = FILES_DIR_NAME + "/." + name + "_lockstate";
     }
 
-    public Fichier(String name){
+    public Fichier(String name) {
         this.name = name;
         this.content = new byte[0];
-        this.lock = FILES_DIR_NAME+"/."+name+"_lockstate";
+        this.lock = FILES_DIR_NAME + "/." + name + "_lockstate";
     }
 
     @Override
@@ -34,28 +39,26 @@ public class Fichier implements Serializable{
         return String.format("* %-20s%s", name, lockState() ? "vérouillé par : " + lockByUser : "non vérouillé");
     }
 
-    public boolean lockState(){
+    public boolean lockState() {
         try {
             FileReader fileReader = new FileReader(lock);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             String[] state = new String[2];
-            for(int i=0; i<2; i++) {
+            for (int i = 0; i < 2; i++) {
                 state[i] = bufferedReader.readLine();
-            }   
+            }
             bufferedReader.close();
             lockByUser = state[1];
-            return Boolean.parseBoolean(state[0]);         
-        }
-        catch(FileNotFoundException e) {
-            System.out.println("Unable to open file '" + lock + "'");                
-        }
-        catch(IOException e) {
+            return Boolean.parseBoolean(state[0]);
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to open file '" + lock + "'");
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return false;
     }
 
-    private boolean changeLockState(String state, String username){
+    private boolean changeLockState(String state, String username) {
         try {
             FileWriter fileWriter = new FileWriter(lock);
             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
@@ -64,23 +67,22 @@ public class Fichier implements Serializable{
             bufferedWriter.write(username);
             bufferedWriter.close();
             return true;
-        }
-        catch(IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return false;
     }
 
-    public boolean lock_fichier(Account account){
-        if(!lockState()){
+    public boolean lock_fichier(Account account) {
+        if (!lockState()) {
             lockByUser = account.userName;
             return changeLockState(String.valueOf(true), lockByUser);
         }
         return false;
     }
 
-    public boolean unlock_fichier(Account account){
-        if(lockByUser.equals(account.userName)){
+    public boolean unlock_fichier(Account account) {
+        if (lockByUser.equals(account.userName)) {
             lockByUser = "";
             return changeLockState(String.valueOf(false), lockByUser);
         }
