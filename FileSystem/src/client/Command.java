0e77b793f5353b0a16f68a7d2592f1fc798cdb80
Fudgyking <escalado19@hotmail.com>
2018-09-29 12:29:04 -0400

    Fix exception handling in PushCommand

--- FileSystem/src/client/Command.java
+++ FileSystem/src/client/Command.java
@@ -1,12 +1,13 @@
 package client;
 
+import java.rmi.RemoteException;
+
 import shared.Account;
-import java.io.IOException;
 import shared.AuthServerInterface;
 import shared.FileServerInterface;
 
 public abstract class Command{
 
-    public abstract void run(Account account, FileServerInterface fileServer, String fileName) throws IOException;
+    public abstract void run(Account account, FileServerInterface fileServer, String fileName) throws RemoteException;
 
 }
