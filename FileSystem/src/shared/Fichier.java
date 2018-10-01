23ed7d1e43c0305bf1cdf8bc4522babd6a16532a
quochaotran <quochao.tran.up@gmail.com>
2018-10-01 16:14:03 -0400

    relocate lock files

--- FileSystem/src/shared/Fichier.java
+++ FileSystem/src/shared/Fichier.java
@@ -13,7 +13,7 @@ public class Fichier implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    private static final String FILES_DIR_NAME = "files";
+    private static final String FILES_DIR_NAME = ".lock";
 
     public String name;
     public byte[] content;
