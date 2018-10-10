262bbd6ba0ee9ddfac895d77320bb211bf54a48e
quochaotran <quochao.tran.up@gmail.com>
2018-10-10 01:37:05 -0400

    adding Response

---
+++ TP2/src/shared/Response.java
@@ -0,0 +1,11 @@
+package shared;
+
+public class Response{
+    public String filename;
+    public int res;
+
+    public Response(String filename, int res){
+        this.filename = filename;
+        this.res = res;
+    }
+}
