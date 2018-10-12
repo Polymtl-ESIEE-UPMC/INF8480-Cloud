5230649ac1c74785f298e020b69b722a50b3e70c
Fudgyking <escalado19@hotmail.com>
2018-10-11 20:26:35 -0400

    Liaison de Operations et OperationToDo

--- TP2/src/shared/OperationTodo.java
+++ TP2/src/shared/OperationTodo.java
@@ -1,11 +1,19 @@
 package shared;
 
-public class OperationTodo{
+public class OperationTodo {
     public String name;
     public int parameter;
-    
-    public OperationTodo(String name, int parameter){
+
+    public OperationTodo(String name, int parameter) {
         this.name = name;
         this.parameter = parameter;
-    } 
+    }
+
+    public int execute() {
+        if (name.equals("pell")) {
+            return Operations.pell(parameter);
+        } else {
+            return Operations.prime(parameter);
+        }
+    }
 }
