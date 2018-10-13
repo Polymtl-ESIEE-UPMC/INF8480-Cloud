baed286109f7f5db8bb58f72b468fb0b6a0ebbaf
Fudgyking <escalado19@hotmail.com>
2018-10-13 15:34:58 -0400

    Fix script calculationServer.sh

--- TP2/calculationServer.sh
+++ TP2/calculationServer.sh
@@ -22,4 +22,4 @@ java -cp "$basepath"/server.jar:"$basepath"/shared.jar \
   -Djava.rmi.server.codebase=file:"$basepath"/shared.jar \
   -Djava.security.policy="$basepath"/policy \
   -Djava.rmi.server.hostname="$IPADDR" \
-  server.CalculationServer
+  server.CalculationServer $*
