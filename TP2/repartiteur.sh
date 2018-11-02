bb9ced76250b25425734861d5a8bb60283dc905d
Vincent <vincent.chasse@polymtl.ca>
2018-11-02 17:04:54 -0400

    Fix script de repartiteur.sh

--- TP2/repartiteur.sh
+++ TP2/repartiteur.sh
@@ -4,23 +4,7 @@ pushd $(dirname $0) > /dev/null
 basepath=$(pwd)
 popd > /dev/null
 
-cat << EndOfMessage
-HELP: 
-./server.sh -ip ip_address
-	- ip_address: (OPTIONAL) L'addresse ip du serveur.
-	  Si l'arguement est non fourni, on conisdère que le serveur est local (ip_address = 127.0.0.1)
-
-EndOfMessage
-
-OPTION=$1
-IPADDR="127.0.0.1"
-if [ "$1" = "-ip" ]
-  then
-    IPADDR=$2
-fi
-
 java -cp "$basepath"/server.jar:"$basepath"/shared.jar \
   -Djava.rmi.server.codebase=file:"$basepath"/shared.jar \
   -Djava.security.policy="$basepath"/policy \
-  -Djava.rmi.server.hostname="$IPADDR" \
   server.Repartiteur $*
